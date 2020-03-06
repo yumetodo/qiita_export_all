4828a5d5b2fa4b368abec687f9276d47638f7276
yumetodo <yume-wikijp@live.jp>
2020-03-06 21:33:20 +0900

    feat(test): add comment test

--- test/test.js
+++ test/test.js
@@ -4,14 +4,26 @@ const nock = require("nock");
 const path = require("path");
 const QiitaApi = require("../bin/qiita");
 const Item = require("../bin/item.js");
+// eslint-disable-next-line no-unused-vars
+const Comment = require("../bin/comment.js");
 const ImageManager = require("../bin/image-manager.js");
 
-/**
- * @param {nock.Scope} scope
- */
-const registerItemMockImpl = scope => {
+test.before(() => {
+  console.log("nock");
+  const mock = nock("https://qiita.com")
+    .persist()
+    .get("/api/v2/items/bd41f31f39dd590e8c80/comments")
+    .query(q => "page" in q && q.page === "1")
+    .replyWithFile(200, path.join(__dirname, "comments1.json"), {
+      "content-type": "application/json; charset=utf-8",
+      link:
+        '<https://qiita.com/api/v2/items/bd41f31f39dd590e8c80/comments?page=1>; rel="first", ' +
+        '<https://qiita.com/api/v2/items/bd41f31f39dd590e8c80/comments?page=1>; rel="last"',
+      "rate-limit": 1000,
+      "rate-remaining": 1000,
+    });
   for (let i = 1; i <= 6; ++i) {
-    scope
+    mock
       .get("/api/v2/users/yumetodo/items")
       .query(q => "page" in q && q.page === `${i}`)
       .replyWithFile(200, path.join(__dirname, `items${i}.json`), {
@@ -25,20 +37,26 @@ const registerItemMockImpl = scope => {
         "rate-remaining": 1000,
       });
   }
-  return scope;
-};
+});
+test.after.always(() => {
+  console.log("clean nock");
+  nock.cleanAll();
+});
 test("item", async t => {
-  const mock = nock("https://qiita.com");
-  registerItemMockImpl(mock);
   const qiita = new QiitaApi("xxx");
   const rawItems = await qiita.GetAllUserItems("yumetodo");
   const ids = ["48d77f5d554df84f66f7", "bd41f31f39dd590e8c80"];
   const items = rawItems.map(i => new Item(i)).filter(i => ids.includes(i.id));
   t.is(items.length, ids.length);
 });
+test("comment", async t => {
+  const qiita = new QiitaApi("xxx");
+  const rawItems = await qiita.GetAllUserItems("yumetodo");
+  const item = rawItems.map(i => new Item(i)).filter(i => i.id === "bd41f31f39dd590e8c80")[0];
+  await item.FetchComments(qiita);
+  t.true(item.comments.map(c => c.id).includes("616b3d36e3ecd3ce2d91"));
+});
 test("image register", async t => {
-  const mock = nock("https://qiita.com");
-  registerItemMockImpl(mock);
   const qiita = new QiitaApi("xxx");
   const rawItems = await qiita.GetAllUserItems("yumetodo");
   const item = rawItems.map(i => new Item(i)).filter(i => i.id === "bd41f31f39dd590e8c80")[0];
