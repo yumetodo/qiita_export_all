f81d7a6cb2d5ddf3ff11eccdf00faa1f7e1765eb
yumetodo <yume-wikijp@live.jp>
2020-03-07 18:57:24 +0900

    1.4.1

--- bin/index.js
+++ bin/index.js
@@ -66,7 +66,7 @@ if (token == null) {
   console.error("Fail to find QIITA_ACCESS_TOKEN env");
 } else {
   program
-    .version("1.4.0")
+    .version("1.4.1")
     .name("qiita_export_all")
     .option("-u, --user-id <id>", "Qiita user id you want to download(default: the user who get QIITA_ACCESS_TOKEN).")
     .option("-o, --output <path>", "Write output to <path> instead of current directory.")
